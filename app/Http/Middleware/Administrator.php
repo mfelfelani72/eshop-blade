73f2c8525c35f15c5732c77391c54d1294f3ade7
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-04-23 21:50:59 +0400

    stsrt view front

--- app/Http/Middleware/Administrator.php
+++ app/Http/Middleware/Administrator.php
@@ -36,7 +36,7 @@ class Administrator
         );
         
 
-        $routeFrom = app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
+        // $routeFrom = app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
 dd("sadsa");
         if (Auth::user()) {
 dd('yes');
