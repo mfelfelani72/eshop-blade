016a7dd857b05b79de2b676200e7e1d123d4dd5d
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-05-16 17:07:17 +0400

    start ltr rtl front

--- resources/views/layouts/front/tag-head.blade.php
+++ resources/views/layouts/front/tag-head.blade.php
@@ -1,6 +1,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce Website</title>
-    <link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
+
+     {{-- for set ltr or rtl style --}}
+    @if (in_array(Session::get('locale'), Config::get('config-app.rtl-langs')))
+        <link rel="stylesheet" href="{{ asset('front/css/rtl-style.css') }}">
+    @else
+        <link rel="stylesheet" href="{{ asset('front/css/ltr-style.css') }}">
+    @endif
+    {{-- for set ltr or rtl style --}}
+
+
+    {{-- <link rel="stylesheet" href="{{ asset('front/css/style.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('front/fonts/remixicon.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
