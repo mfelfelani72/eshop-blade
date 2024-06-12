9ecdce0fe32657d74de0e2c5594c884bac545c27
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-06-12 11:47:23 +0400

    menu

---
+++ database/migrations/2024_06_03_081643_header_menu.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('header_menu', function (Blueprint $table) {
+
+            $table->id();
+
+            // $table->bigInteger('product_id')->unsigned()->index()->nullable();
+            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->date('title');
+            $table->string('code');
+            $table->string('link');
+            $table->string('lable');
+            $table->string('operator');
+            $table->string('extra');
+            $table->enum('status', ['enable', 'disable', 'deleted']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('header_menu');
+    }
+};
