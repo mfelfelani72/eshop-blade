5c06e41c0c2a6581421029da41a340a47ec75242
radmehr <raouf.radmehr@gmail.com>
2024-06-17 15:08:38 +0330

    radmehr-translate

--- lang/fa/dashboard.php
+++ lang/fa/dashboard.php
@@ -1,5 +1,34 @@
 <?php
 return [
+
+    // sidebar
+
     'create' => 'ساختن' ,
+    'shop' => 'فروشگاه' ,
+    'index' => 'وارد کنید' ,
+    'category' => 'دسته بندی' ,
+    'products' => 'محصولات' ,
+'product trend' => 'روند محصول' ,
+'product futured' => 'محصول آینده' ,
+'settings' => 'تنظیمات' ,
+'pannel admin' => 'پنل مدیریت' ,
+'change logo' => 'تغییر لوگو' ,
+'public front' => 'جبهه عمومی' ,
+'slider' => 'منوی لغزنده' ,
+'banner' => 'پوسته' ,
+'Cloner team admin' => 'مدیریت تیم کلونر' ,
+'Made with' => 'ساخته شده با' ,
+'by Cloner team' => 'توسط تیم کلونر' ,
+'all rights reserved 2024' => 'تمامی حقوق محفوظ است 2024',
+
+// sidebar
+
+// header
+
+
+
+// header
+
+
 ];
 
