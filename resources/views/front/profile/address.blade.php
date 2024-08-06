adbf78e681f51cc4924f6c21ec94330996c56d56
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-08-06 11:41:07 +0400

    asd

--- resources/views/front/profile/address.blade.php
+++ resources/views/front/profile/address.blade.php
@@ -1,33 +1,9 @@
 <div class="content-body">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="profile card card-body px-3 pt-3 pb-0">
-                    <div class="profile-head">
-                        <div class="photo-content">
-                            <div class="cover-photo rounded" style="background: url({{ asset($userProfile->cover) }})">
-                            </div>
-                        </div>
-                        <div class="profile-info">
-                            <div class="profile-photo">
-                                <img src={{ asset($userProfile->image) }} class="img-fluid rounded-circle"
-                                    alt="">
-                            </div>
-                            <div class="profile-details">
-                                <div class="profile-name px-3 pt-2">
-                                    <h4 class="text-primary mb-0">{{ $userProfile->user->name }}</h4>
-                                    <p>{{ $userProfile->bio }}</p>
-                                </div>
-                                <div class="profile-email px-2 pt-2">
-                                    <h4 class="text-muted mb-0">Role</h4>
-                                    <p>{{ Auth::user()->role }}</p>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @php
+               echo view('front/profile/header-user-profile', compact('userProfile'))->render();
+            @endphp
             <div class="col-xl-12">
                 <div class="card">
                     <div class="card-body">
@@ -53,8 +29,7 @@
                                         $active = 'active show';
                                     @endphp
                                     @foreach ($userProfileAddress as $row)
-                                        <div id="address-{{ $row->id }}"
-                                            class="tab-pane {{ $active }} fade">
+                                        <div id="address-{{ $row->id }}" class="tab-pane {{ $active }} fade">
                                             <div class="my-post-content pt-3 mx-1">
                                                 <h4 class="text-primary mb-4">Full Address</h4>
                                                 <div class="row">
@@ -132,7 +107,8 @@
                                                         </div>
                                                         <div class="row mb-2">
                                                             <div class="col-sm-4 col-5">
-                                                                <h5 class="f-w-500">Floor <span class="pull-end">:</span>
+                                                                <h5 class="f-w-500">Floor <span
+                                                                        class="pull-end">:</span>
                                                                 </h5>
                                                             </div>
                                                             <div class="col-sm-8 col-7">
@@ -141,8 +117,7 @@
                                                         </div>
                                                         <div class="row mb-2">
                                                             <div class="col-sm-4 col-5">
-                                                                <h5 class="f-w-500">Unit <span
-                                                                        class="pull-end">:</span>
+                                                                <h5 class="f-w-500">Unit <span class="pull-end">:</span>
                                                                 </h5>
                                                             </div>
                                                             <div class="col-sm-8 col-7">
---
+++ resources/views/front/profile/header-user-profile.blade.php
@@ -0,0 +1,26 @@
+ <div class="col-lg-12">
+     <div class="profile card card-body px-3 pt-3 pb-0">
+         <div class="profile-head">
+             <div class="photo-content">
+                 <div class="cover-photo rounded" style="background: url({{ asset($userProfile->cover) }})">
+                 </div>
+             </div>
+             <div class="profile-info">
+                 <div class="profile-photo">
+                     <img src={{ asset($userProfile->image) }} class="img-fluid rounded-circle" alt="">
+                 </div>
+                 <div class="profile-details">
+                     <div class="profile-name px-3 pt-2">
+                         <h4 class="text-primary mb-0">{{ $userProfile->user->name }}</h4>
+                         <p>{{ $userProfile->bio }}</p>
+                     </div>
+                     <div class="profile-email px-2 pt-2">
+                         <h4 class="text-muted mb-0">Role</h4>
+                         <p>{{ Auth::user()->role }}</p>
+                     </div>
+
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
